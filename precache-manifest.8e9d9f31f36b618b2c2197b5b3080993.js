d3b0fefe34c5de0788d73c0ecb3ffb727c259c82
chanceDira <chancedesire60@gmail.com>
2021-12-02 14:07:16 +0200

    Updates

---
+++ precache-manifest.8e9d9f31f36b618b2c2197b5b3080993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af880bfb7ef8fad008c2",
+    "url": "/Final-Year-Project/static/css/main.7cfd1e79.chunk.css"
+  },
+  {
+    "revision": "af880bfb7ef8fad008c2",
+    "url": "/Final-Year-Project/static/js/main.af880bfb.chunk.js"
+  },
+  {
+    "revision": "c502052d1d2796b5ce69",
+    "url": "/Final-Year-Project/static/js/runtime~main.c502052d.js"
+  },
+  {
+    "revision": "f9bb5c136c0361dd3ac5",
+    "url": "/Final-Year-Project/static/css/2.9dd924f8.chunk.css"
+  },
+  {
+    "revision": "f9bb5c136c0361dd3ac5",
+    "url": "/Final-Year-Project/static/js/2.f9bb5c13.chunk.js"
+  },
+  {
+    "revision": "d7634e774cf335cf7ae7ed253de64e7d",
+    "url": "/Final-Year-Project/index.html"
+  }
+];
